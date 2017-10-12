0dff25781a9386469dab146a61c7460031775450
CharleyXu <xucharlie1993@gmail.com>
2017-10-12 19:06:42 +0800

    message entities created

---
+++ src/main/java/com/xu/tulingchat/bean/message/TextMessage.java
@@ -0,0 +1,18 @@
+package com.xu.tulingchat.bean.message;
+
+/**
+ * 文本消息
+ */
+public class TextMessage extends BaseMessage {
+    //文本内容
+    private String Content;
+
+    public String getContent() {
+        return Content;
+    }
+
+    public void setContent(String content) {
+        Content = content;
+    }
+
+}
