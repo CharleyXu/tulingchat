4e7345afb8dadfeeeb53c4a6cd51e07c67f34b46
CharleyXu <xucharlie1993@gmail.com>
2017-11-20 19:29:53 +0800

    update iputil

--- src/main/java/com/xu/tulingchat/util/IpUtil.java
+++ src/main/java/com/xu/tulingchat/util/IpUtil.java
@@ -1,11 +1,15 @@
 package com.xu.tulingchat.util;
 
+import com.google.common.base.Strings;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.InetSocketAddress;
 import java.net.Proxy;
 import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -72,37 +76,38 @@ public class IpUtil {
 		if (b == false) {
 			return;
 		}
-		Map<String, String> headers = new HashMap<>();
-		headers.put("Referer", "http://music.163.com/discover/artist");
-		headers.put("Host", "music.163.com");
-		headers.put("Origin", "http://music.163.com");
-		headers.put("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36");
-		String result = HttpClientUtil.sendProxyGet("113.83.240.249", 53281, "http://music.163.com/discover/artist/cat?id=1001&initial=65", headers, null);
-		System.out.println(result);
-		headers.clear();
-		headers.put("Referer", "https://www.baidu.com/");
-		headers.put("User-Agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36");
-//		headers.put("","");
-		String s = HttpClientUtil.sendGet("https://www.baidu.com/", null, null);
-		System.out.println(s);
-//		Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("219.136.172.180", 9797));
-//		Document document = null;
-//		try {
-//			document = Jsoup.connect("http://music.163.com/discover/artist/cat?id=1001&initial=65").proxy(proxy)
-//					.header("Referer", "http://music.163.com/discover/artist")
-//					.header("User-Agent",
-//							"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36")
-//					.header("Host", "music.163.com")
-//					.header("Origin", "http://music.163.com").get();
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
-//		if (document == null || Strings.isNullOrEmpty(document.head().data())) {
-//			System.out.println("-----");
-//		} else {
-////			System.out.println(document.body().text());
-//			document.select("a.f-thide").stream().map(w -> w.text() + "-->" + w.attr("href")).forEach(System.out::println);
-//		}
+//		Map<String, String> headers = new HashMap<>();
+//		headers.put("Referer", "http://music.163.com/discover/artist");
+//		headers.put("Host", "music.163.com");
+//		headers.put("Origin", "http://music.163.com");
+//		headers.put("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36");
+//		String result = HttpClientUtil.sendProxyGet("113.83.240.249", 53281, "http://music.163.com/discover/artist/cat?id=1001&initial=65", headers, null);
+//		System.out.println(result);
+//		headers.clear();
+//		headers.put("Referer", "https://www.baidu.com/");
+//		headers.put("User-Agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36");
+////		headers.put("","");
+//		String s = HttpClientUtil.sendGet("https://www.baidu.com/", null, null);
+//		System.out.println(s);
+
+		Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("219.136.172.180", 9797));
+		Document document = null;
+		try {
+			document = Jsoup.connect("http://music.163.com/discover/artist/cat?id=1001&initial=65")
+					.header("Referer", "http://music.163.com/discover/artist")
+					.header("User-Agent",
+							"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36")
+					.header("Host", "music.163.com")
+					.header("Origin", "http://music.163.com").get();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		if (document == null || Strings.isNullOrEmpty(document.head().data())) {
+			System.out.println("-----");
+		} else {
+			System.out.println(document.body().text());
+			document.select("a.f-thide").stream().map(w -> w.text() + "-->" + w.attr("href")).forEach(System.out::println);
+		}
 
 	}
 }
